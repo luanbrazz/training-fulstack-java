1bfa2abb91764f23461475ab62b0259700411b8c
Luan Braz <luan_silvacpv@hotmail.com>
2023-09-25 07:28:30 -0300

    criado classe Acesso.java

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -0,0 +1,54 @@
+package jdev.mentoria.lojavirtual.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "acesso")
+//permite que você crie valores de chave primária sequenciais automaticamente ao persistir objetos da entidade.
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Override
+    public String getAuthority() {
+        return this.descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acesso acesso = (Acesso) o;
+        return Objects.equals(id, acesso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
