7d49374dbfdad95b4ae373b8baec244a717d9e76
Luan Braz <luan_silvacpv@hotmail.com>
2023-10-01 19:09:24 -0300

    criado classe NotaFiscalVenda e StatusRastreio

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalVenda.java
@@ -0,0 +1,87 @@
+package jdev.mentoria.lojavirtual.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "nota_fiscal_venda")
+@SequenceGenerator(name = "seq_nota_fiscal_venda", sequenceName = "seq_nota_fiscal_venda", allocationSize = 1, initialValue = 1)
+public class NotaFiscalVenda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_venda")
+    private Long id;
+
+    private String numeroNota;
+    private String serieNota;
+    private String tipo;
+
+    @Column(columnDefinition = "text")
+    private String xml;
+    @Column(columnDefinition = "text")
+    private String pdf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroNota() {
+        return numeroNota;
+    }
+
+    public void setNumeroNota(String numeroNota) {
+        this.numeroNota = numeroNota;
+    }
+
+    public String getSerieNota() {
+        return serieNota;
+    }
+
+    public void setSerieNota(String serieNota) {
+        this.serieNota = serieNota;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
+
+    public String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotaFiscalVenda that = (NotaFiscalVenda) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
