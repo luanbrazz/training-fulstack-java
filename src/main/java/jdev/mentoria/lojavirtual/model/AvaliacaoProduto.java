8c0718656236f33ae1f52a39971b8773d87ceb45
Luan Braz <luan_silvacpv@hotmail.com>
2023-10-02 21:58:48 -0300

    criado AvaliacaoProduto, ItemVendaLoja e VendaCompraLojaVirtual

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/AvaliacaoProduto.java
@@ -0,0 +1,86 @@
+package jdev.mentoria.lojavirtual.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "avaliacao_produto")
+@SequenceGenerator(name = "seq_avaliacao_produto", sequenceName = "seq_avaliacao_produto", allocationSize = 1, initialValue = 1)
+public class AvaliacaoProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+    private Long id;
+
+    private Integer nota;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(
+            name = "pessoa_id", // Nome da coluna no banco de dados que armazenará a chave estrangeira.
+            nullable = false,   // Indica que a coluna "pessoa_id" não pode conter valores nulos.
+            foreignKey = @ForeignKey(
+                    value = ConstraintMode.CONSTRAINT, // Define o modo de restrição da chave estrangeira.
+                    name = "pessoa_fk" // Nome da restrição de chave estrangeira no banco de dados.
+            )
+    )
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "produto_id", // Nome da coluna no banco de dados que armazenará a chave estrangeira.
+            nullable = false,   // Indica que a coluna "produto_id" não pode conter valores nulos.
+            foreignKey = @ForeignKey(
+                    value = ConstraintMode.CONSTRAINT, // Define o modo de restrição da chave estrangeira.
+                    name = "produto_fk" // Nome da restrição de chave estrangeira no banco de dados.
+            )
+    )
+    private Produto produto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvaliacaoProduto that = (AvaliacaoProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
