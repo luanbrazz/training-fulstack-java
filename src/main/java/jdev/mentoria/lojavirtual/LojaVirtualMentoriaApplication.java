1d887b152994e06bd7c31f9e6c4c6f3eab2cf3fb
Luan Braz <luan_silvacpv@hotmail.com>
2024-02-08 12:13:47 -0300

    config application.properties

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,14 @@ package jdev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
+@EnableJpaRepositories(basePackages = "jdev.mentoria.lojavirtual.repository")
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
     public static void main(String[] args) {
