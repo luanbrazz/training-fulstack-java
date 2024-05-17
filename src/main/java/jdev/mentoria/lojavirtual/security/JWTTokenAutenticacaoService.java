bd926722a7b98e11753bf7d14b1dcacafa6e9007
Luan Braz <luan_silvacpv@hotmail.com>
2024-05-17 11:21:20 -0300

    Spring Security com Json Web Token - geração do token e evitar cors na web

--- src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -30,7 +30,28 @@ public class JWTTokenAutenticacaoService {
 
         response.addHeader(HEADER_STRING, token);
 
+        liberacaoCors(response);
+
         response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
     }
 
+    /*fazendo liberação contra erro de cors no navegador*/
+    private void liberacaoCors(HttpServletResponse response) {
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
+
+    }
 }
