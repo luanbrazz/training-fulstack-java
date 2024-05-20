073de1b5159cb3081a5b204b73fead97d2821e18
Luan Braz <luan_silvacpv@hotmail.com>
2024-05-20 10:50:47 -0300

    Spring Security com Json Web Token - jwtApiAutenticacaoFilter para interceptar todas as requisições do sistema para validar jwt

---
+++ src/main/java/jdev/mentoria/lojavirtual/security/JWTLoginFilter.java
@@ -0,0 +1,49 @@
+package jdev.mentoria.lojavirtual.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdev.mentoria.lojavirtual.model.Usuario;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    /*configurando o gerenciador de autenticacao*/
+    public JWTLoginFilter(String url, AuthenticationManager authenticationManager) {
+
+        /*obriga autenticar a url*/
+        super(new AntPathRequestMatcher(url));
+
+        /*gerenciador de autenticacao*/
+        setAuthenticationManager(authenticationManager);
+    }
+
+    /*retorna o usuario ao processar a autenticação*/
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        Usuario user = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+
+        /*retorna o user com login e senha*/
+        return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(user.getLogin(), user.getSenha()));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        try {
+            new JWTTokenAutenticacaoService().addAuthentication(response, authResult.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/jdev/mentoria/lojavirtual/security/JwtApiAutenticacaoFilter.java
@@ -0,0 +1,29 @@
+package jdev.mentoria.lojavirtual.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*FILTRO ONDE TODAS AS REQUISIÇÕES SERAO CAPTURADAS PARA AUTENTICAR*/
+public class JwtApiAutenticacaoFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        /*Estabelece a autenticacao do user*/
+        Authentication authentication = new JWTTokenAutenticacaoService()
+                .getAuthentication((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse);
+
+        /*coloca o processo de autenticacao para o spring security*/
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
